7241fb8e4b682f7f0c87807f37e8b60adf1d064b
moRagabmo12 <moragabmohamed12@gmail.com>
2025-11-28 14:43:55 +0200

    initial upload

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash_View/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: SplashView());
+  }
+}
